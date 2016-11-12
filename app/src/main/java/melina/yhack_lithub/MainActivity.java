e7d36292eb85aaa2d03bfd1f885d8fc9c65dd241
Melina Delgado <melina.delgado@yale.edu>
2016-11-12 16:01:51 -0500

    hi

--- app/src/main/java/melina/yhack_lithub/MainActivity.java
+++ app/src/main/java/melina/yhack_lithub/MainActivity.java
@@ -17,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMapView = (MapView)findViewById(R.id.map);
+        mLocationLayer = new GraphicsLayer();
+        mMapView.addLayer(mLocationLayer);
     }
 }
