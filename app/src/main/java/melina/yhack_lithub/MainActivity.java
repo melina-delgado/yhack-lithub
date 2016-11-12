47a27a6b937e0c107c848401c2f6233d05cbad5f
Melina Delgado <melina.delgado@yale.edu>
2016-11-12 15:15:54 -0500

    hello

---
+++ app/src/main/java/melina/yhack_lithub/MainActivity.java
@@ -0,0 +1,21 @@
+package melina.yhack_lithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.esri.android.map.GraphicsLayer;
+import com.esri.android.map.MapView;
+import com.esri.core.geometry.Point;
+
+public class MainActivity extends AppCompatActivity {
+    MapView mMapView;
+    GraphicsLayer mLocationLayer;
+    Point mLocationLayerPoint;
+    String mLocationLayerPointString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
